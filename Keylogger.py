cf0e32728f89f2ba80ced9a22752efffecd4089c
Bobio Wononuah <138823983+bandmanbo@users.noreply.github.com>
2023-11-12 16:14:24 -0500

    Initial commit

---
+++ Keylogger.py
@@ -0,0 +1,19 @@
+#reading key strokes that write to a txt file.
+#File handling - how to read, write and append to a file.   
+
+#first parameter in open is name of file we are writing to
+#seceond parameter in open is 'mode' which determines how the code will interact with the file
+#r - reading 
+#w - writing 
+#a - appending
+
+f = open("log.txt", "a")
+#filedata = f.read()
+#print(filedata)
+f.write("\n We be gettin that wizard bread fasho \n")
+f.close
+
+
+#first we create a variable named 'f' for our open function. set the parameters. 
+#then we tell our variable 'f' to to either read or write 'log.txt'
+#\n is to append text to a new line.
