7d6e582c38f479ce92b877b1f510daf4f68d936a
Bobio Wononuah <138823983+bandmanbo@users.noreply.github.com>
2023-11-12 16:22:21 -0500

    Keylogger Lesson 1
    Using the "open()" function to create and interact with a log file

--- Keylogger.py
+++ Keylogger.py
@@ -1,7 +1,7 @@
-#reading key strokes that write to a txt file.
+#Goals: reading key strokes that write to a txt file.
 #File handling - how to read, write and append to a file.   
 
-#first parameter in open is name of file we are writing to
+#first parameter in open is to (create the) name the file we are writing to
 #seceond parameter in open is 'mode' which determines how the code will interact with the file
 #r - reading 
 #w - writing 
@@ -16,4 +16,4 @@ f.close
 
 #first we create a variable named 'f' for our open function. set the parameters. 
 #then we tell our variable 'f' to to either read or write 'log.txt'
-#\n is to append text to a new line.
+#\n to append text to a new line.
